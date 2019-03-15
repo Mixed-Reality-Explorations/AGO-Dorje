050f5c99a764bb65f84193a560bf789d23105ebd
molecule <molecule@berkeley.edu>
2019-03-15 11:38:16 -0700

    dictationtest works, my dictationdemo doesnt

---
+++ latest-holotoolkit/Assets/ColorAndDictationManipulation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAndDictationManipulation : MonoBehaviour
+{
+
+    private new Renderer renderer;
+    public GameObject objToBeManipulated;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
